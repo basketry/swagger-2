d5970f5e64722218978a8280db6333cc1db89d49
Steve Konves <stephen.konves@gmail.com>
2022-04-24 17:55:29 -0700

    refactor: parse using json-to-ast

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { Parser } from 'basketry';
 
-import { OAS2Parser } from './parser';
+import { OAS2Parser } from './parser-v2';
 
 const parser: Parser = (input) => {
-  return new OAS2Parser(JSON.parse(input)).parse();
+  return new OAS2Parser(input).parse();
 };
 
 export default parser;
