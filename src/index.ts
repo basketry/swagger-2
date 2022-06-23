9535c8f1b91eadd5291276a52b682f706ddf5bfb
Steve Konves <stephen.konves@gmail.com>
2022-06-22 18:25:57 -0700

    feat: parse meta

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import { Parser } from 'basketry';
 
 import { OAS2Parser } from './parser';
 
-const parser: Parser = (input) => {
-  return { service: new OAS2Parser(input).parse(), violations: [] };
+const parser: Parser = (input, sourcePath) => {
+  return { service: new OAS2Parser(input, sourcePath).parse(), violations: [] };
 };
 
 export default parser;
